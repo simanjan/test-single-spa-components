8305a9556d90a51f689909d033be4e443d4dbb5a
Alex Simanovich <asimanovich@eisgroup.com>
2020-05-24 13:15:52 +0300

    Initial commit

---
+++ src/extrascript-components.js
@@ -0,0 +1,39 @@
+import {
+  Button,
+  Layout,
+  Menu,
+  Breadcrumb,
+  Tooltip,
+  Form,
+  Input,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  AutoComplete
+} from "antd";
+import "./set-public-path";
+import "antd/dist/antd.css";
+
+// Anything exported from this file is importable by other in-browser modules.
+// export function publicApiFunction() {}
+
+const { Item } = Menu;
+
+export {
+  Button,
+  Layout,
+  Menu,
+  Item as MenuItem,
+  Breadcrumb,
+  Tooltip,
+  Form,
+  Input,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  AutoComplete
+};
